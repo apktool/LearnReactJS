46366a571be5af08ec54fd793d2ed1d70f7dcd48
apktool <apktoolOo@gmail.com>
2019-03-06 22:56:07 +0800

    Event and state attribute

--- rails365/chapter3/src/components/Home.js
+++ rails365/chapter3/src/components/Home.js
@@ -2,20 +2,35 @@ import React, {Component} from 'react';
 import PropTypes from 'prop-types';
 
 export default class Home extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            age: props.age
+        };
+        this.age = this.props.age;
+    }
+
+    onMakeOlder() {
+        this.setState(
+            {age: this.state.age + 3}
+        )
+    }
+
     render() {
         let content = "";
 
         if (content.length === 0) {
             content = "Hello Home";
+            console.info(this);
         }
-        console.log(this.props);
-
         return (
             <div className="container">
                 <div className="row">
                     <div className="col-xs-1 col-xs-offset-11">
                         <h1>{content}</h1>
-                        <div> your name is {this.props.name}, your age is {this.props.age}</div>
+                        <div> your name is {this.props.name}, your age is {this.state.age}</div>
+                        {/*<button onClick={this.onMakeOlder.bind(this)} className="btn btn-primary">Make me older</button>*/}
+                        <button onClick={() => this.onMakeOlder()} className="btn btn-primary">Make me older</button>
                     </div>
                 </div>
                 <div className="SecondRow">
