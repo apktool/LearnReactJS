34271054261db8cc52e48106a929a25b81df623b
apktool <apktoolOo@gmail.com>
2020-07-06 23:26:11 +0800

    [ADD] react hooks demo

---
+++ reacthooks/my-app-01/src/App.js
@@ -0,0 +1,19 @@
+import React, {useState} from 'react';
+
+function App() {
+    const [count, setCount] = useState(0)
+
+    return (
+        <div>
+            <p>You Clicked {count} times</p>
+            <button onClick={
+                () => {
+                    setCount(count + 1)
+                }}>
+                Click Me
+            </button>
+        </div>
+    )
+}
+
+export default App;
